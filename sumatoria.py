0e356e612f60fd67cdf2dd923b274d5b1640f843
Tomas Acuna <volcan8@yahoo.es>
2020-09-23 19:29:18 -0600

    sumatoria.py

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,22 @@
+
+
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    r = 0;
+    for i in range (1,n+1):
+        for j in range (1,i+1):
+            for k in range (j,i+j+1):
+                r += 1;
+    return r
+
+
+
+
+
+
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+	k = ((3*n)*(n+1)/2)
+	return int (k)
+    #raise NotImplementedError()
 
